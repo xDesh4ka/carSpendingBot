bf428331cd01ad5dc600f0c43c87dc2e5b331023
Аитбаев Вадим <v.aitbaev@bftcom.com>
2021-08-08 21:46:22 +0500

    early woking version

---
+++ src/main/kotlin/database/DatabaseFunctions.kt
@@ -0,0 +1,53 @@
+package database
+
+import botTg.*
+import com.github.kotlintelegrambot.entities.Message
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+
+
+fun getAllUserIds() : MutableList<Long>
+{
+    val userIds = mutableListOf<Long>()
+    Class.forName("com.mysql.cj.jdbc.Driver")
+    val connection  = DriverManager.getConnection(url, "root", "")
+    val stmt: PreparedStatement = connection.prepareStatement("select * from user")
+    val rs = stmt.executeQuery()
+    while(rs.next())
+    {
+        userIds.add(rs.getLong(1))
+    }
+    return userIds;
+}
+
+fun getTotalSumById(id : Long) : Long
+{
+    var ret : Long = 0
+    Class.forName("com.mysql.cj.jdbc.Driver")
+    val connection  = DriverManager.getConnection(url, "root", "")
+    val stmt: PreparedStatement = connection.prepareStatement("select sum(cost) from spend where user_id = ?")
+    stmt.setLong(1, id)
+    val rs = stmt.executeQuery()
+    while(rs.next())
+    {
+        ret = rs.getLong(1)
+    }
+    return ret
+}
+
+fun addSpend(message : Message) : Unit {
+    Class.forName("com.mysql.cj.jdbc.Driver")
+    val connection  = DriverManager.getConnection(url, "root", "")
+    val text = message.text!!
+    val spend = Spend(
+        (text.split(" ") - text.split(" ").last()).joinToString(" "),
+        text.split("""(.*) """.toRegex()).last().toLong()
+    )
+    val stmt: PreparedStatement = connection.prepareStatement(
+        "insert into spend (description, cost, user_id) values (? , ?, ?)"
+    )
+    stmt.setString(1, spend.description)
+    stmt.setLong(2, spend.cost)
+    stmt.setLong(3, message.chat.id)
+    stmt.executeUpdate()
+}
---
+++ src/main/kotlin/database/UserDao.kt
@@ -0,0 +1,96 @@
+package database
+
+import botTg.*
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
+val url = "jdbc:mysql://localhost:3306/tgbotTest?verifyServerCertificate=false&useSSL=false&serverTimezone=UTC"
+
+
+fun ResultSet.getSpend(id : Int) : MutableList<Spend> {
+    val buf = mutableListOf<Spend>()
+    Class.forName("com.mysql.cj.jdbc.Driver")
+    val connection  = DriverManager.getConnection(url, "root", "")
+    val stmt: PreparedStatement = connection.prepareStatement("select * from spend where user_id=?")
+    stmt.setInt(1, id)
+    val rs = stmt.executeQuery()
+    while (rs.next())
+    {
+        buf.add(Spend(rs.getString(1), rs.getLong(2)))
+    }
+    return buf
+}
+
+fun PreparedStatement.setSpend(saveOrUpdate : Boolean, user : User) : Unit
+{
+    Class.forName("com.mysql.cj.jdbc.Driver")
+    val connection  = DriverManager.getConnection(url, "root", "")
+    val stmt: PreparedStatement = if (saveOrUpdate) {
+        connection.prepareStatement(
+            "insert into spend (description, cost, user_id) values (? , ?, ?) "
+        )
+    }
+    else {
+        val newstmt  = connection.prepareStatement("delete from spend where user_id = ${user.id}")
+        newstmt.executeUpdate()
+        connection.prepareStatement(
+            "insert into spend (description, cost, user_id) values (? , ?, ?)"
+        )
+    };
+    user.spends?.forEach {
+        stmt.setString(1, it.description)
+        stmt.setLong(2, it.cost)
+        stmt.setLong(3, user.id)
+        stmt.executeUpdate()
+    }
+}
+
+class UserDao
+{
+    fun getById(id: Int) : User?
+    {
+        Class.forName("com.mysql.cj.jdbc.Driver")
+        val connection  = DriverManager.getConnection(url, "root", "")
+
+        val stmt: PreparedStatement = connection.prepareStatement("select * from user where id=? limit 1")
+        stmt.setInt(1, id)
+        val rs = stmt.executeQuery()
+
+        var  ret : User? = null
+
+        while (rs.next())
+        {
+            ret = User(
+                rs.getLong(1),
+                rs.getString(2),
+                rs.getSpend(id)
+            )
+        }
+        return ret
+    }
+
+    fun save(user: User) : Unit {
+        Class.forName("com.mysql.cj.jdbc.Driver")
+        val connection  = DriverManager.getConnection(url, "root", "")
+        val stmt: PreparedStatement = connection.prepareStatement(
+            "insert into user (id, name) values (? , ?) "
+        );
+        stmt.setLong(1, user.id)
+        stmt.setString(2, user.name)
+        stmt.executeUpdate()
+        stmt.setSpend(true, user)
+    }
+
+    fun update(user: User) : Unit {
+        Class.forName("com.mysql.cj.jdbc.Driver")
+        val connection  = DriverManager.getConnection(url, "root", "")
+        val stmt: PreparedStatement = connection.prepareStatement(
+            "update user set name =? where id= ?"
+        )
+        stmt.setString(1, user.name)
+        stmt.setLong(2, user.id)
+        stmt.setSpend(false, user)
+        stmt.executeUpdate()
+    }
+}
